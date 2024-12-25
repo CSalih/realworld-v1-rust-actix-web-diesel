25d5b8bb3feb6da0406bc933ff3de3ba68ba5f96
Salih Candir <salih@live.at>
2024-12-26 00:21:02 +0100

    feat: allow any cors origin, header and method

--- src/app/drivers/middlewares/cors.rs
+++ src/app/drivers/middlewares/cors.rs
@@ -1,15 +1,9 @@
-use crate::constants::env_key;
 use actix_cors::Cors;
-use actix_web::http;
-use std::env;
 
 pub fn cors() -> Cors {
-    let frontend_origin = env::var(env_key::FRONTEND_ORIGIN).unwrap_or_else(|_| "*".to_string());
-
     Cors::default()
-        .allowed_origin(&frontend_origin)
-        .allowed_origin_fn(|origin, _req_head| origin.as_bytes().ends_with(b".rust-lang.org"))
-        .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
-        .allowed_header(http::header::CONTENT_TYPE)
+        .allow_any_method()
+        .allow_any_origin()
+        .allow_any_header()
         .max_age(3600)
 }
